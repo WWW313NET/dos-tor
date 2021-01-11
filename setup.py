4c9a3fd62945957d0eecbb733da97bf3d87c5fe5
WWW313NET <75071180+WWW313NET@users.noreply.github.com>
2021-01-11 10:42:24 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import os
-os.system("sudo apt install tor")
-os.system("sudo apt install proxychains4")
-os.system("sudo apt install python3")
-os.system("sudo apt install python3-pip")
+os.system("sudo apt install tor -y")
+os.system("sudo apt install proxychains4 -y")
+os.system("sudo apt install python3 -y")
+os.system("sudo apt install python3-pip -y")
 os.system("sudo pip3 install scapy")
